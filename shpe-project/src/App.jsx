ab156e36d18045eee2fc9dc599073f817d2a83f6
CarsonRivera04 <carsonrivera04@gmail.com>
2025-01-31 18:28:10 -0500

    Added a description for the app

--- shpe-project/src/App.jsx
+++ shpe-project/src/App.jsx
@@ -16,7 +16,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + SHPE</h1>
+      <p>Learn Spanish and Portuguese</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
