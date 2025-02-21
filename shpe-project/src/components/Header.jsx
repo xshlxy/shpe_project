ec45b2d320ea4542bbaa4fdeaaa396c01c84db4a
CarsonRivera04 <carsonrivera04@gmail.com>
2025-02-21 17:55:22 -0500

    Removed everything

---
+++ shpe-project/src/components/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+    return (
+        <>
+            <ul>
+                <li>Home</li>
+                <li>Language</li>
+                <li>Culture</li>
+                <li>Profile</li>
+            </ul>
+        </>
+    );
+}
+
+export default Header;
