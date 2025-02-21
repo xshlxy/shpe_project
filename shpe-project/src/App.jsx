0580a56d3516e0b2e6ffa183c7d5a2d1a61cbef6
Ashley <ashley.oliveiraa@outlook.com>
2025-02-21 18:25:46 -0500

    Changes home page

--- shpe-project/src/App.jsx
+++ shpe-project/src/App.jsx
@@ -2,7 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { Link } from "react-router";
-import Header from './components/Header'
+import Header from './components/Header';
+import Home from './pages/Home';
+import React from 'react'
 import './App.css'
 
 
@@ -10,6 +12,7 @@ function App() {
   return (
     <>
       <Header />
+      <Home />
     </>
   );
 }
--- shpe-project/src/pages/Home.jsx
+++ shpe-project/src/pages/Home.jsx
@@ -1,10 +1,11 @@
-import "React" from "react";
+import React from "react";
 
 const Home = () => {
+
     return (
         <div className="home-page">
             <h1>Welcome to Your Learning Adventure!</h1>
-            <p>Helping Latin-American kids adapt to American culture and langugae through fun learning tools.</p>
+            <p>Helping Latin-American kids adapt to American culture and language through fun learning tools.</p>
             <button>Start Learning</button>
         </div>
     );
