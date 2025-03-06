1380483451039d7e81e709581d9fc3ed2e6de263
Carson <carsonrivera04@gmail.com>
2025-03-06 07:49:47 -0500

    Worked on navbar and mainpage

--- shpe-project/src/components/Header.jsx
+++ shpe-project/src/components/Header.jsx
@@ -3,6 +3,7 @@ const Header = () => {
     return (
         <>
             <ul>
+                <img src="https://shpe.org/wp-content/uploads/2022/06/shpe-logo.png" alt="SHPE Logo" /> 
                 <li>Home</li>
                 <li>Language</li>
                 <li>Culture</li>
--- shpe-project/src/pages/Home.jsx
+++ shpe-project/src/pages/Home.jsx
@@ -1,13 +1,23 @@
 import React from "react";
+import kidsLearning from "../assets/kids-learning.png";
+import "./Home.css";
 
 const Home = () => {
 
     return (
-        <div className="home-page">
-            <h1>Welcome to Your Learning Adventure!</h1>
-            <p>Helping Latin-American kids adapt to American culture and language through fun learning tools.</p>
-            <button>Start Learning</button>
-        </div>
+        <>
+            <div className="container">
+                <div className="box left">
+                    <div classname="centerContainer">
+                        <h1>Welcome to Your Learning Adventure!</h1>
+                        <p>Helping Latin-American kids adapt to American culture and language through fun learning tools.</p>
+                        <button>Start Learning</button>
+                    </div>
+                </div>
+                <div className="box right">
+                </div>
+            </div>
+        </>
     );
 }
 
